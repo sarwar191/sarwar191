c93451dc92419dee6b8fc3175dcc428d6146fcd3
sarwar191 <92360462+sarwar191@users.noreply.github.com>
2021-12-14 22:55:38 +0530

    Create 10.c

---
+++ 10.c
@@ -0,0 +1,18 @@
+//Cse-19-37
+
+#include <stdio.h>
+int main() 
+{
+    int a, b,result=1,i=1;
+    printf("Enter a base number: ");
+    scanf("%d", &a);
+    printf("Enter an exponent: ");
+    scanf("%d", &b);
+    while (i<=b)
+   {
+        result=result* a;
+        i++;
+    }
+    printf("Answer = %d", result);
+    return 0;
+}
